65e99509bbe61244f5669be4a7e92a5f5384e3ee
David Lgc <legouellecd@gmail.com>
2024-01-23 18:28:05 +0100

    Add confetti Animation + fix bug

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let scores, currentScore, activePlayer, gamePlaying;
+let confetti = [];
+let animationFrameId;
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 init();
 
@@ -8,7 +15,7 @@ document.getElementById("roll-dice").addEventListener("click", function () {
 
     let diceImage = document.getElementById("dice-image");
     diceImage.style.animation = "none";
-    diceImage.src = './img/Dice-' + dice + '.svg';
+    diceImage.src = "./img/Dice-" + dice + ".svg";
 
     if (dice !== 1) {
       currentScore += dice;
@@ -26,12 +33,14 @@ document.getElementById("hold").addEventListener("click", function () {
     document.getElementById("global-score-" + activePlayer).textContent =
       scores[activePlayer - 1];
 
-    if (scores[activePlayer - 1] >= 50) {
-      alert("Joueur " + activePlayer + " a gagné !");
-      gamePlaying = false;
-    } else {
-      nextPlayer();
-    }
+    checkWin();
+
+    //   if (scores[activePlayer - 1] >= 50) {
+    //     alert("Joueur " + activePlayer + " a gagné !");
+    //     gamePlaying = false;
+    //   } else {
+    //     nextPlayer();
+    //   }
   }
 });
 
@@ -40,18 +49,19 @@ document.getElementById("new-game").addEventListener("click", init);
 function updateActivePlayer() {
   document.getElementById("player1").classList.remove("active");
   document.getElementById("player2").classList.remove("active");
-
   document.getElementById("player" + activePlayer).classList.add("active");
 }
 
 function init() {
+  stopConfetti();
 
-  let player1Name = prompt("Nom du joueur1","Player 1");
-  let player2Name = prompt("Nom du joueur2","Player 2");
+  let player1Name = prompt("Nom du joueur1", "Player 1");
+  let player2Name = prompt("Nom du joueur2", "Player 2");
 
-  document.getElementById("player1").querySelector("h2").textContent = player1Name
-
-  document.getElementById("player2").querySelector("h2").textContent = player2Name
+  document.getElementById("player1").querySelector("h2").textContent =
+    player1Name;
+  document.getElementById("player2").querySelector("h2").textContent =
+    player2Name;
 
   scores = [0, 0];
   activePlayer = 1;
@@ -63,7 +73,6 @@ function init() {
   document.getElementById("current-score-1").textContent = "0";
   document.getElementById("current-score-2").textContent = "0";
   updateActivePlayer();
-
 }
 
 function nextPlayer() {
@@ -75,82 +84,80 @@ function nextPlayer() {
   updateActivePlayer();
 }
 
-
 function checkWin() {
-  // Vérifier si le joueur a gagné
   if (scores[activePlayer - 1] >= 50) {
     alert("Joueur " + activePlayer + " a gagné !");
-    initConfetti(); 
+    initConfetti();
+    render();
     gamePlaying = false;
   } else {
     nextPlayer();
   }
 }
 
-
-
-
-
-
 //-----------Confetti js--------------
 //-----------Var Inits--------------
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-cx = ctx.canvas.width / 2;
-cy = ctx.canvas.height / 2;
+// canvas = document.getElementById("canvas");
+// ctx = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// cx = ctx.canvas.width / 2;
+// cy = ctx.canvas.height / 2;
 
-let confetti = [];
 const confettiCount = 300;
 const gravity = 0.5;
 const terminalVelocity = 5;
 const drag = 0.075;
 const colors = [
-{ front: 'red', back: 'darkred' },
-{ front: 'green', back: 'darkgreen' },
-{ front: 'blue', back: 'darkblue' },
-{ front: 'yellow', back: 'darkyellow' },
-{ front: 'orange', back: 'darkorange' },
-{ front: 'pink', back: 'darkpink' },
-{ front: 'purple', back: 'darkpurple' },
-{ front: 'turquoise', back: 'darkturquoise' }];
-
+  { front: "red", back: "darkred" },
+  { front: "green", back: "darkgreen" },
+  { front: "blue", back: "darkblue" },
+  { front: "yellow", back: "darkyellow" },
+  { front: "orange", back: "darkorange" },
+  { front: "pink", back: "darkpink" },
+  { front: "purple", back: "darkpurple" },
+  { front: "turquoise", back: "darkturquoise" },
+];
 
 //-----------Functions--------------
 resizeCanvas = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  cx = ctx.canvas.width / 2;
-  cy = ctx.canvas.height / 2;
+  // cx = ctx.canvas.width / 2;
+  // cy = ctx.canvas.height / 2;
 };
 
 randomRange = (min, max) => Math.random() * (max - min) + min;
 
-initConfetti = () => {
+function initConfetti() {
+  confetti = [];
   for (let i = 0; i < confettiCount; i++) {
     confetti.push({
       color: colors[Math.floor(randomRange(0, colors.length))],
       dimensions: {
         x: randomRange(10, 20),
-        y: randomRange(10, 30) },
+        y: randomRange(10, 30),
+      },
 
       position: {
         x: randomRange(0, canvas.width),
-        y: canvas.height - 1 },
+        y: canvas.height - 1,
+      },
 
       rotation: randomRange(0, 2 * Math.PI),
       scale: {
         x: 1,
-        y: 1 },
+        y: 1,
+      },
 
       velocity: {
         x: randomRange(-25, 25),
-        y: randomRange(0, -50) } });
-
-
+        y: randomRange(0, -50),
+      },
+    });
   }
-};
+  canvas.style.pointerEvents = "none";
+}
 
 //---------Render-----------
 render = () => {
@@ -166,7 +173,10 @@ render = () => {
 
     // Apply forces to velocity
     confetto.velocity.x -= confetto.velocity.x * drag;
-    confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
+    confetto.velocity.y = Math.min(
+      confetto.velocity.y + gravity,
+      terminalVelocity
+    );
     confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
 
     // Set position
@@ -182,7 +192,8 @@ render = () => {
 
     // Spin confetto by scaling y
     confetto.scale.y = Math.cos(confetto.position.y * 0.1);
-    ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
+    ctx.fillStyle =
+      confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
 
     // Draw confetto
     ctx.fillRect(-width / 2, -height / 2, width, height);
@@ -191,24 +202,35 @@ render = () => {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
   });
 
-  // Fire off another round of confetti
-  if (confetti.length <= 10) initConfetti();
 
-  window.requestAnimationFrame(render);
+  if (confetti.length > 0) {
+    animationFrameId = window.requestAnimationFrame(render);
+  } else {
+    animationFrameId = null;
+  }
+
+  // Fire off another round of confetti
+  // if (confetti.length <= 10) initConfetti();
+  // window.requestAnimationFrame(render);
+  // animationFrameId = window.requestAnimationFrame(render);
 };
 
-//---------Execution--------
-initConfetti();
-render();
+function stopConfetti() {
+  if (animationFrameId) {
+    window.cancelAnimationFrame(animationFrameId);
+    animationFrameId = null;
+  }
+  confetti = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.style.pointerEvents = "auto";
+}
 
 //----------Resize----------
-window.addEventListener('resize', function () {
+window.addEventListener("resize", function () {
   resizeCanvas();
 });
 
 //------------Click------------
-window.addEventListener('click', function () {
+window.addEventListener("click", function () {
   initConfetti();
 });
-// animation de dé 
-// menu dépliant pour les regles tout en haut
